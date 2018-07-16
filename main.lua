b8f671fb7336a1cfd20f45e68d03bb89654a64ac
Simon Rivest <simrivest@gmail.com>
2018-07-16 18:17:13 -0400

    Adding color to powerbar

--- main.lua
+++ main.lua
@@ -5,18 +5,32 @@ print("Hello " .. UnitName("player"));
 -- Addon Initialisation
 --------------------------------------------------------------------------------------------------------------
 
---Getting Mainpower information
-mainPower = {"0", "1", "2", "3", "4", "5", "12"}
-for  i = 1, 7 do
-	
+--Array for Color by power
+powerColor = {
+    {"0","0","0","1"}, -- mana
+	{"1","1","0","0"}, -- rage
+	{"2","1","0.5","0.25"}, -- focus
+	{"3","1","1","0"}, -- energy
+	{"4","0","0.96","0.41"}, -- combo point
+	{"5","0.5","0.5","0.5"}, -- Runes
+	{"6","0","0.82","1"}, -- Runic Power
+	{"7","0.5","0.32","0.55"}, -- soul shards
+	{"8","0.3","0.52","0.9"}, -- lunar power
+	{"9","0.95","0.90","0.60"}, -- holy power
+	{"10","0","0.5","1"}, -- Alternate power (i think its not use)
+	{"11","0","0.5","1"}, -- maelstrom
+	{"12","0.71","1","0.92"}, -- chi
+	{"13","0.4","0","0.8"}, -- insanity
+	{"14","0","1","0"}, -- obsolete
+	{"15","0","1","0"}, -- obsolete 2
+	{"16","0.1","0.1","0.98"}, -- arcane charge
+	{"17","0.78","0.26","0.99"}, -- Fury
+    {"18","1","0.61","0"} -- Pain
+    }
 
-local pm = UnitPowerMax("player",mainPower[i])	
-local pt = mainPower[i]	
-	if pm > 0  then
-		PlayerMainPowerType = pt
-		break
-   end
-end
+
+
+
 
 --------------------------------------------------------------------------------------------------------------
 -- General fonction
@@ -48,7 +62,59 @@ local function ReadableNumber(num, places)
     return ret
 end
 
+--Getting player powertype information
 
+--Getting Mainpower information
+mainPower = {"0", "1", "2", "3", "5", "12", "17", "18"}
+for  i = 1, 8 do
+local pm = UnitPowerMax("player",mainPower[i])	
+local pt = mainPower[i]	
+	if pm > 0  then
+		playerMainPowerType = pt
+		break
+   end
+end
+--Getting bar color for mainpower 
+
+
+for i = 1, 18 do
+local powerIndex = powerColor[i]
+local colorIndex = powerIndex[1]
+	if colorIndex == playerMainPowerType then
+	mainPowerColorR = powerIndex[2]
+	mainPowerColorG = powerIndex[3]
+	mainPowerColorB = powerIndex[4]
+	end
+
+	
+end
+
+--Getting secondaryPower information
+secondaryPower = {"4", "6", "7", "8", "9", "11", "13", "16"}
+for  i = 1, 8 do
+local pm = UnitPowerMax("player",secondaryPower[i])	
+local pt = secondaryPower[i]	
+	if pm > 0  then
+		secondaryPowerType = pt
+		--print (secondaryPowerType)
+		break
+   end
+end
+
+--Getting bar color for secondaryPower 
+for i = 1, 18 do
+local colorIndex = powerColor[i]
+local powerIndex = colorIndex[1]
+	if powerIndex == secondaryPowerType then
+	secondaryPowerColorR = colorIndex[2]
+	secondaryPowerColorG = colorIndex[3]
+	secondaryPowerColorB = colorIndex[4]
+	print (mainPowerColorR)
+	print (mainPowerColorG) 
+	print (mainPowerColorB) 	
+	break
+	end
+end
 --------------------------------------------------------------------------------------------------------------
 -- player healt bar
 --------------------------------------------------------------------------------------------------------------
@@ -88,7 +154,7 @@ pStatusbar.text:SetText(ReadableNumber(UnitHealth(self.unit)))
 end)
 
 --------------------------------------------------------------------------------------------------------------
--- player Power bar
+-- player mainPower bar
 --------------------------------------------------------------------------------------------------------------
 
 --Drawing player Power bar
@@ -101,7 +167,7 @@ pPowerBar:SetPoint("CENTER",-300,-20)
 pPowerBar:SetSize(150,10)
 pPowerBar:Show()
 pPowerBar:SetValue(100)
-pPowerBar:SetStatusBarColor(0,1,1, 1)
+pPowerBar:SetStatusBarColor(mainPowerColorR,mainPowerColorG,mainPowerColorB)
 pPowerBar.text = pPowerBar:CreateFontString(nil, "OVERLAY", "GameFontHighlightLarge")
 pPowerBar.text:SetAllPoints()
 
@@ -119,7 +185,44 @@ pPowerBarBg:SetBackdropColor(0,0,0,1);
 
 --Run Script uptade a l'event OnUpdate (chaque Frame).
 pPowerBar:SetScript("OnUpdate",function(self) 
-self:SetValue(UnitPower("player",mainPower[PlayerMainPowerType])/UnitPowerMax("player",mainPower[PlayerMainPowerType])*100) 
+self:SetValue(UnitPower("player",playerMainPowerType)/UnitPowerMax("player",playerMainPowerType)*100) 
+
+
+end)
+
+--------------------------------------------------------------------------------------------------------------
+-- player secondaryPower bar
+--------------------------------------------------------------------------------------------------------------
+
+--Drawing player secondary Power bar
+playerPowerBar2 = CreateFrame("Statusbar",nil,UIParent)
+playerPowerBar2:SetStatusBarTexture("Interface\\TARGETINGFRAME\\UI-StatusBar")
+local pPowerBar2 = playerPowerBar2
+pPowerBar2.unit = "player"
+pPowerBar2:SetMinMaxValues(0,100)
+pPowerBar2:SetPoint("CENTER",-300,-40)
+pPowerBar2:SetSize(150,10)
+pPowerBar2:Show()
+pPowerBar2:SetValue(100)
+pPowerBar2:SetStatusBarColor(secondaryPowerColorR,secondaryPowerColorG,secondaryPowerColorB)
+pPowerBar2.text = pPowerBar2:CreateFontString(nil, "OVERLAY", "GameFontHighlightLarge")
+pPowerBar2.text:SetAllPoints()
+
+--Drawing  Background and border under the secondary /Power Bar
+playerPowerBar2Bg = CreateFrame("Statusbar",BORDER,UIParent)
+local pPowerBar2Bg = playerPowerBar2Bg
+pPowerBar2Bg:SetPoint("CENTER",-300,-40)
+pPowerBar2Bg:SetSize(160,20)
+pPowerBar2Bg:Show()
+pPowerBar2Bg:SetBackdrop({bgFile = "Interface/Tooltips/UI-Tooltip-Background", 
+                                            edgeFile = "Interface/Tooltips/UI-Tooltip-Border", 
+                                            tile = true, tileSize = 16, edgeSize = 16, 
+                                            insets = { left = 4, right = 4, top = 4, bottom = 4 }});
+pPowerBar2Bg:SetBackdropColor(0,0,0,1);
+
+--Run Script uptade a l'event OnUpdate (chaque Frame).
+pPowerBar2:SetScript("OnUpdate",function(self) 
+self:SetValue(UnitPower("player",secondaryPowerType)/UnitPowerMax("player",secondaryPowerType)*100) 
 
 
 end)
@@ -199,7 +302,7 @@ end)
 --main power 0,1,2,3,5,12,17,18
 --secondary 6,7,8,9,13,14,15
 
-secondaryPower = {"6", "7", "8", "9", "13", "14", "15", "16", "17"}
+
 
 
 
